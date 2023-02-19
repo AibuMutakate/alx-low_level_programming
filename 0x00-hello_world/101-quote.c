ea8958c1d34e627279c14ee17602eda5441cfd43
AibuMutakate <aibumutakate995@gmail.com>
2023-02-18 19:54:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Return: Always 1
+ *Return: Always 1 (Failure)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	ltrace(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
