bd930176cd80684460c98c8dcd89e2c53dc136af
AibuMutakate <aibumutakate995@gmail.com>
2023-02-20 09:00:17 -0800

    Write a C program that prints exactly, and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  *main - prints exactly and that piece of art is useful
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
