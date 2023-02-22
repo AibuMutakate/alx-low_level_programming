fa45f9d0003d13f8011cb10f7c7de99cf0e0fa1c
AibuMutakate <aibumutakate995@gmail.com>
2023-02-22 03:26:14 -0800

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - csuses of infinite loop
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	{
+		/*putchar(i);*/
+	}
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
